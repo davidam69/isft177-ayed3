4b97dc34ad825bc16dd2049726666835ba4eff42
davidam69 <david_-m_pericon@hotmail.com>
2023-05-27 18:53:51 -0300

    mi primer commit

---
+++ lista_de_tareas.js
@@ -0,0 +1,22 @@
+let listaTareas = [];
+function agregarTarea() {
+  const tareaInput = document.getElementById("tareaInput");
+  const tareaTexto = tareaInput.value;
+
+  if (tareaTexto.trim() !== "") {
+    listaTareas.push(tareaTexto);
+
+    const listaTareasElemento = document.getElementById("listaTareas");
+    const nuevaTareaElemento = document.createElement("li");
+    nuevaTareaElemento.innerText = tareaTexto;
+
+    nuevaTareaElemento.addEventListener("click", function() {
+      this.classList.toggle("completed");
+    });
+
+    listaTareasElemento.appendChild(nuevaTareaElemento);
+
+    tareaInput.value = "";
+  }
+}
+
