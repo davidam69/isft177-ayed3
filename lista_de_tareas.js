1fc1555683021ddb0063ea09dc427325c05c21d3
damiranda <titoelegipcio@gmail.com>
2023-08-13 16:54:23 -0300

    hago cambios

--- lista_de_tareas.js
+++ lista_de_tareas.js
@@ -1,24 +1,34 @@
-let listaTareas = [];
+const input = document.createElement('input');
+input.type = 'text';
+input.id = 'tareaInput';
+input.placeholder = 'Ingrese una tarea';
+document.body.appendChild(input);
 
+const addButton = document.createElement('button');
+addButton.textContent = 'Agregar Tarea';
+document.body.appendChild(addButton);
+
+const h1 = document.createElement('h1');
+h1.textContent = 'Lista de Tareas';
+document.body.appendChild(h1);
+
+const ul = document.createElement('ul');
+ul.id = 'listaTareas';
+document.body.appendChild(ul);
+
+// Agregar tarea a la lista
 function agregarTarea() {
-  const tareaInput = document.getElementById("tareaInput");
-  const tareaTexto = tareaInput.value;
+  const tareaTexto = input.value.trim();
 
-  if (tareaTexto.trim() !== "") {
-    
-    listaTareas.push(tareaTexto);
-
-    const listaTareasElemento = document.getElementById("listaTareas");
-    const nuevaTareaElemento = document.createElement("li");
-    nuevaTareaElemento.innerText = tareaTexto;
-
-    nuevaTareaElemento.addEventListener("click", function() {
-      this.classList.toggle("completed");
-    });
-
-    listaTareasElemento.appendChild(nuevaTareaElemento);
-
-    tareaInput.value = "";
+  if (tareaTexto !== '') {
+    const nuevaTarea = document.createElement('li');
+    nuevaTarea.textContent = tareaTexto;
+    ul.appendChild(nuevaTarea);
+    input.value = '';
   }
 }
+
+// Agregar tarea cuando se presiona la tecla Enter
+addButton.addEventListener('click', agregarTarea);
+
 
